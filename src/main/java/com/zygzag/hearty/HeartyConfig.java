e7c10ffc1c10fadf01c8e166e5e135f5b249bf22
ZygZagGaming <46930418+ZygZagGaming@users.noreply.github.com>
2022-09-25 17:05:14 -0400

    made render double hearts default to false

--- src/main/java/com/zygzag/hearty/HeartyConfig.java
+++ src/main/java/com/zygzag/hearty/HeartyConfig.java
@@ -9,7 +9,7 @@ public class HeartyConfig {
         WITHER_OVERRIDE_POISON;
     public static void register() {
         ForgeConfigSpec.Builder clientBuilder = new ForgeConfigSpec.Builder();
-        RENDER_DOUBLE_HEARTS = clientBuilder.comment(" Whether or not hearts should store 1 hp instead of the vanilla 2").define("render_double_hearts", true);
+        RENDER_DOUBLE_HEARTS = clientBuilder.comment(" Whether or not hearts should store 1 hp instead of the vanilla 2").define("render_double_hearts", false);
         WITHER_OVERRIDE_POISON = clientBuilder.comment(" Whether or not withered black hearts should show instead of poisoned green hearts when the player has both effects at once. (very minor)").define("wither_override_poison", true);
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, clientBuilder.build());
     }
